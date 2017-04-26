5b0c774f387865dc0a597af90990282eac3b4319
Freddi-kru <Freddi-kru@users.noreply.github.com>
2017-04-27 00:50:01 +0200

    creazione mappa per vlad

---
+++ vagnetti/data/Vladimiro2.js
@@ -0,0 +1,7 @@
+var json_Vladimiro2={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 12.437818161986964, 43.137815985646967 ] } }
+]
+}
